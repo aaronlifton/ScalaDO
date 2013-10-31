51a57dbd8e20891428c543672e02765d5005136f
Edwin Fuquen <efuquen@gmail.com>
2013-10-30 22:44:49 -0400

    proper future testing

--- src/test/scala/com/parascal/scalado/DigitalOceanSpec.scala
+++ src/test/scala/com/parascal/scalado/DigitalOceanSpec.scala
@@ -1,21 +1,23 @@
 package com.parascal.scalado
 
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 import java.util.Properties
 import java.io.{File, FileReader}
 
-class DigitalOceanSpec extends FlatSpec with ShouldMatchers {
+class DigitalOceanSpec extends FlatSpec with ShouldMatchers with ScalaFutures {
   
   val clientId = System.getenv("DO_CLIENT_ID")
   val apiKey = System.getenv("DO_API_KEY")
+  val doService = DigitalOcean(clientId, apiKey)
   
-  "DigitalOcean" should "respond with a list of droplets" ignore {
-    val doService = DigitalOcean(clientId, apiKey)
-    val fut = doService.droplets
-    println(Await.result(fut, Duration.Inf))
+  "DigitalOcean" should "respond with a list of empty droplets" ignore {
+    whenReady(doService.droplets) { droplets =>
+      droplets should have length (0)
+    }
   }
 }
